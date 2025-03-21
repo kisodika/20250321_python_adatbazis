f0bcf855db78e894256d5fd59ac6ba540cf89f44
kisodika <kis.odett@diak.szbi-pg.hu>
2025-03-21 13:31:16 +0100

    Update kiralyok.py

--- kiralyok.py
+++ kiralyok.py
@@ -4,10 +4,30 @@ cnx = connection.MySQLConnection(user='root', password='mysql',
                                  host='127.0.0.1',
                                  database='kiralyok')
 
-
+#Táblák megjelenítése
 cursor = cnx.cursor()
 cursor.execute("SHOW TABLES")
 for table in cursor:
     print(table)
+print("-----------------------")
+
+
+#Uralkodók megjelenítése
+cursor.execute("SELECT * FROM uralkodo")
+for uralkodo in cursor:
+    print(uralkodo)
+print("-----------------------")
+
+#elso matyas megjelenítése
+cursor.execute("SELECT * FROM uralkodo WHERE nev = 'I. Mátyás'")
+for uralkodo in cursor:
+    print(uralkodo)
+print("-----------------------")
+
+#elso matyi dolgai
+cursor.execute("SELECT szul, hal FROM uralkodo WHERE nev = 'I. Mátyás'")
+for uralkodo in cursor:
+    print(f"Mátyás király élt: {uralkodo[0]} - {uralkodo[1]}")
+print("-----------------------")
 
 cnx.close()
